643535d91cba0a40cff62b02bf76d6e6ec3a9452
Enzo Lizama <lizama.enzo@gmail.com>
2020-02-15 16:56:18 -0500

    init android app

---
+++ android_app/app/src/main/java/com/enzoftware/androidappmodule/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.enzoftware.androidappmodule
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+//    lateinit var flutterEngine : FlutterEngine
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+//        // Instantiate a FlutterEngine.
+//        flutterEngine = FlutterEngine(this)
+//
+//        // Start executing Dart code to pre-warm the FlutterEngine.
+//        flutterEngine.dartExecutor.executeDartEntrypoint(
+//            DartExecutor.DartEntrypoint.createDefault()
+//        )
+//
+//        // Cache the FlutterEngine to be used by FlutterActivity.
+//        FlutterEngineCache
+//            .getInstance()
+//            .put("my_engine_id", flutterEngine)
+
+        fab.setOnClickListener {
+          startActivity(
+//              FlutterActivity
+//                  .withCachedEngine("my_engine_id")
+//                  .build(this)
+                      FlutterActivity.createDefaultIntent(this)
+          )
+      }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
