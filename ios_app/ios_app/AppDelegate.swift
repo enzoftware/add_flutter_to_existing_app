f4aac3663147d839d7e95395acef2855cfd54396
Enzo Lizama <lizama.enzo@gmail.com>
2020-02-15 17:32:42 -0500

    init ios app with flutter config

--- ios_app/ios_app/AppDelegate.swift
+++ ios_app/ios_app/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Flutter
+import FlutterPluginRegistrant
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
