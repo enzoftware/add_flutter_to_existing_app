e731a5f7614d078ee9a99ddd1b4d1e68d0cf2c75
Enzo Lizama <lizama.enzo@gmail.com>
2020-02-15 17:04:12 -0500

    init ios app

---
+++ ios_app/ios_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_app
+//
+//  Created by Enzo Lizama on 2/15/20.
+//  Copyright © 2020 Enzo Lizama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
