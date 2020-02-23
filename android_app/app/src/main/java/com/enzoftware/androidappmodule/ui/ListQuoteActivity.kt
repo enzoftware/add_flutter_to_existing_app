1612b844e11cde140a5b6b74b09dc64a220b210d
Enzo Lizama <lizama.enzo@gmail.com>
2020-02-23 01:19:08 -0500

    work with android fetch list elements

--- android_app/app/src/main/java/com/enzoftware/androidappmodule/ui/ListQuoteActivity.kt
+++ android_app/app/src/main/java/com/enzoftware/androidappmodule/ui/ListQuoteActivity.kt
@@ -3,57 +3,65 @@ package com.enzoftware.androidappmodule.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.enzoftware.androidappmodule.R
+import com.enzoftware.androidappmodule.databinding.ContentMainBinding
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.content_main.view.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class ListQuoteActivity : AppCompatActivity() {
 
-//    lateinit var flutterEngine : FlutterEngine
+    lateinit var flutterEngine : FlutterEngine
+    private val quoteViewModel : ListQuoteViewModel by viewModel()
+    private lateinit var adapter: QuoteAdapter
+    private lateinit var mBinding: ContentMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.content_main)
+        adapter = QuoteAdapter()
+        mBinding.recyclerViewQuotes.layoutManager = LinearLayoutManager(this)
+        mBinding.recyclerViewQuotes.adapter = adapter
+        quoteViewModel.fetchSimpsonsQuotes()
+        quoteViewModel.quotes.observe(this, Observer {
+            adapter.addQuotes(it)
+        })
+        quoteViewModel.loading.observe(this, Observer {
+            if(it){
+                mBinding.pbLoading.visibility = View.VISIBLE
+            } else {
+                mBinding.pbLoading.visibility = View.INVISIBLE
+            }
+        })
 
-//        // Instantiate a FlutterEngine.
-//        flutterEngine = FlutterEngine(this)
-//
-//        // Start executing Dart code to pre-warm the FlutterEngine.
-//        flutterEngine.dartExecutor.executeDartEntrypoint(
-//            DartExecutor.DartEntrypoint.createDefault()
-//        )
-//
-//        // Cache the FlutterEngine to be used by FlutterActivity.
-//        FlutterEngineCache
-//            .getInstance()
-//            .put("my_engine_id", flutterEngine)
 
-        fab.setOnClickListener {
-          startActivity(
-//              FlutterActivity
-//                  .withCachedEngine("my_engine_id")
-//                  .build(this)
-                      FlutterActivity.createDefaultIntent(this)
-          )
-      }
+        // Instantiate a FlutterEngine.
+        flutterEngine = FlutterEngine(this)
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+            .getInstance()
+            .put("my_engine_id", flutterEngine)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
--- android_app/app/src/main/java/com/enzoftware/androidappmodule/ui/QuoteAdapter.kt
+++ android_app/app/src/main/java/com/enzoftware/androidappmodule/ui/QuoteAdapter.kt
@@ -1,7 +1,57 @@
 package com.enzoftware.androidappmodule.ui
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.enzoftware.androidappmodule.R
+import com.enzoftware.androidappmodule.databinding.QuoteItemBinding
+import com.enzoftware.androidappmodule.model.SimpsonsQuote
+import io.flutter.embedding.android.FlutterActivity
+
 
 /**
  * Created by Enzo Lizama Paredes on 2020-02-19.
  * Contact: lizama.enzo@gmail.com
  */
+
+class QuoteAdapter : RecyclerView.Adapter<QuoteAdapter.QuoteViewHolder>(){
+
+    private val quotes = ArrayList<SimpsonsQuote>()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.quote_item, parent, false)
+        return QuoteViewHolder(itemView, parent.context)
+    }
+
+    override fun getItemCount(): Int = quotes.size
+
+    override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
+        holder.bindQuote(quotes[position])
+    }
+
+    fun addQuotes(listQuotes: List<SimpsonsQuote>) {
+        this.quotes.addAll(listQuotes)
+        notifyDataSetChanged()
+    }
+
+    class QuoteViewHolder(itemView: View, private val context: Context): RecyclerView.ViewHolder(itemView){
+
+        fun bindQuote(quote: SimpsonsQuote){
+            val binding = DataBindingUtil.bind<QuoteItemBinding>(itemView)
+            binding?.quote = quote
+            binding?.quoteCardView?.setOnClickListener {
+                startActivity(
+                    context,
+                    FlutterActivity.createDefaultIntent(context),
+                    null
+                )
+            }
+        }
+
+    }
+}
