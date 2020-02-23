d52b1b3e42d305281df3891b545628113173136f
Enzo Lizama <lizama.enzo@gmail.com>
2020-02-23 01:55:21 -0500

    fetch simpsons data from network via swift ui

--- AddFlutter2iOSApp/AddFlutter2iOSApp/ContentView.swift
+++ AddFlutter2iOSApp/AddFlutter2iOSApp/ContentView.swift
@@ -8,9 +8,59 @@
 
 import SwiftUI
 
+struct QuoteEntity: Codable, Identifiable {
+    public var id: String
+    
+    public var character: String
+    public var image: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "quote"
+        
+        case character = "character"
+        case image = "image"
+    }
+}
+
+class FetchQuotes : ObservableObject{
+    @Published
+    var quotes = [QuoteEntity]()
+    init() {
+        let url = URL(string: "https://thesimpsonsquoteapi.glitch.me/quotes?count=10")!
+        URLSession.shared.dataTask(with: url){(data, response, error) in
+            do{
+                if let quoteData = data {
+                    let decodeData = try JSONDecoder().decode([QuoteEntity].self, from: quoteData)
+                    DispatchQueue.main.async {
+                        self.quotes = decodeData
+                    }
+                } else {
+                    print("No data")
+                }
+            }catch{
+                print("Error")
+            }
+            
+        }.resume()
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject
+    var fetch = FetchQuotes()
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            List(fetch.quotes){ quote in
+                VStack(alignment: .leading){
+                    Text(quote.character).font(.system(size: 18))
+                    .foregroundColor(Color.black)
+                    Text(quote.id).font(.system(size: 12))
+                    .foregroundColor(Color.black)
+                }
+                
+            }
+        }
     }
 }
 
